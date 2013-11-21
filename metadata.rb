ee7044bd8707dd5b51fedb9bdf947d339fa1801f
Eric Internicola <einternicola@callisto.(none)>
2013-11-20 21:57:00 -0700

    Initial cut of the Chef Recipe to pull in and configure the Mongo Database.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'setup-mongo'
+maintainer       'TSI'
+maintainer_email 'intere@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures setup-mongo'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,34 @@
+#
+# Cookbook Name:: setup-mongo
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+# Pull the tarball to /tmp:
+remote_file "/tmp/hwlegends.tgz" do
+  source "https://intere.servebeer.com/hwlegends/hwlegends.tgz"
+  mode 00644
+end
+
+# Unpack it:
+bash "unpack tarball" do
+  cwd "/tmp"
+  code <<-EOS
+    tar xzf hwlegends.tgz
+  EOS
+end
+
+# import it:
+bash "import tarball to mongo" do
+  cwd "/tmp"
+  code <<-EOS
+    mongorestore hwlegends/hwlegends
+  EOS
+end
+
+
+
